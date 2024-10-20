b88d1e1bf536b3ea57913c541e772961a207e350
Michael Kuzmin <michael@mkuzmin.io>
2024-10-20 22:39:37 +0300

    Filter types which require a custom query

--- list-types/main.go
+++ list-types/main.go
@@ -40,13 +40,13 @@ func getCfTypes(region string) []string {
 	cfg.Region = region
 
 	var cfTypes []string
-	println("\n=== Mutable")
+	println("=== Mutable")
 	cfTypes = append(cfTypes, getCfTypesProvisioning(cfg, types.ProvisioningTypeFullyMutable)...)
 
-	println("\n=== Immutable")
+	println("=== Immutable")
 	cfTypes = append(cfTypes, getCfTypesProvisioning(cfg, types.ProvisioningTypeImmutable)...)
 
-	println("\n=== Non-provisionable")
+	println("=== Non-provisionable")
 	cfTypes = append(cfTypes, getCfTypesProvisioning(cfg, types.ProvisioningTypeNonProvisionable)...)
 
 	return cfTypes
@@ -77,8 +77,6 @@ func getCfTypesProvisioning(cfg aws.Config, provisioningType types.ProvisioningT
 				log.Fatal("List type is nil")
 			}
 
-			print("\r" + *ts.TypeName + "                                                     ")
-
 			if isListable(cf, *ts.TypeName) {
 				cfTypes = append(cfTypes, *ts.TypeName)
 			}
@@ -94,7 +92,7 @@ func isListable(cf *cloudformation.Client, cfType string) bool {
 		TypeName: &cfType,
 	}
 
-	time.Sleep(500 * time.Millisecond) // prevent API throttling
+	time.Sleep(200 * time.Millisecond) // prevent API throttling
 	describeType, err := cf.DescribeType(ctx, &typeInput)
 	if err != nil {
 		log.Fatal("Cannot describe type", err)
@@ -109,10 +107,16 @@ func isListable(cf *cloudformation.Client, cfType string) bool {
 		log.Fatal("Error parsing JSON: ", err)
 	}
 
-	if _, exists := r.Handlers["list"]; exists {
-		return true
+	if _, exists := r.Handlers["list"]; !exists {
+		println("- " + cfType)
+		return false
 	}
 
-	println()
-	return false
+	if r.Handlers["list"].HandlerSchema != nil {
+		println("* " + cfType)
+		return false
+	}
+
+	print(cfType + "\r")
+	return true
 }
